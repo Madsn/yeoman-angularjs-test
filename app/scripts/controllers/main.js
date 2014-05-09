5fb0b7543b0349d33589cf4613873f4a7a5c4c7f
Mikkel Madsen <m@madsn.net>
2014-05-09 15:20:59 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('systematicEmployeesClubApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
